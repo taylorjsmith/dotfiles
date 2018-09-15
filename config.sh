fc474b21b75a8cc20d1954c78d4cb1ef0eb5f58b
Taylor Smith <taylorsmith2@me.com>
2018-09-14 20:00:34 -0400

    Update config script

--- config.sh
+++ config.sh
@@ -7,13 +7,23 @@
 ###################################
 
 # variables
-dir=~/.dotfiles			# dotfiles directory
-olddir=~/.dotfiles_old		# old dotfiles backup directory
+dir=~/.dotfiles             # dotfiles directory
+olddir=~/.dotfiles_old      # old dotfiles backup directory
 files=".bash_aliases .bash_profile .bashrc .gitconfig .gitignore .vim .vimrc"
 
-# create .dotfiles_old directory
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+# create .dotfiles_old directory if it does not exist already
+if [ ! -d $olddir ]; then
+    echo "Creating $olddir for backup of any existing dotfiles in ~"
+    mkdir -p $olddir
+fi
+
+# unlink existing symlinks if they exist already
+for file in $files; do
+    if [ -L ~/$file ]; then
+        echo "Unlinking existing symlink to $file"
+        unlink ~/$file
+    fi
+done
 
 # change to .dotfiles directory
 echo "Changing to the $dir directory"
@@ -21,8 +31,10 @@ cd $dir
 
 # move existing dotfiles in ~ to .dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/.dotfiles_old/
+    if [ -f ~/$file ]; then
+        echo "Moving $file from ~ to $olddir"
+        mv ~/$file ~/$olddir
+    fi
     echo "Creating symlink to $file in home directory"
     ln -s $dir/$file ~/$file
 done
